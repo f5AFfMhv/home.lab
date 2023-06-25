45e0636efbdefb0f133787246f3cc269b946d5d6
mj <mj@home.lab>
2023-06-25 23:41:47 +0300

    add portainer agent

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,17 +14,17 @@ variable "template_name" {
 }
 
 variable "vm_count" {
-  default     = 5
+  default     = 4
   description = "Number of VMs to create"
 }
 
 variable "vm_name" {
-  default     = ["k3s-cp", "k3s-w1", "k3s-w2", "k3s-w3", "docker"]
+  default     = ["k3s-cp", "k3s-w1", "k3s-w2", "docker"]
   description = "VM name"
 }
 
 variable "vm_cpu" {
-  default     = [2, 2, 2, 2, 4]
+  default     = [2, 2, 2, 4]
   description = "VM cpu cores"
 }
 
@@ -34,17 +34,17 @@ variable "vm_storage" {
 }
 
 variable "vm_disk" {
-  default     = ["10G", "10G", "10G", "10G", "20G"]
+  default     = ["10G", "10G", "10G", "20G"]
   description = "VM disk size"
 }
 
 variable "vm_memory" {
-  default     = [2048, 2048, 2048, 2048, 4096]
+  default     = [2048, 2048, 2048, 4096]
   description = "VM memory in MB"
 }
 
 variable "vm_ip_addresses" {
-  default     = ["192.168.1.30", "192.168.1.31", "192.168.1.32", "192.168.1.33", "192.168.1.34"]
+  default     = ["192.168.1.30", "192.168.1.31", "192.168.1.32", "192.168.1.34"]
   description = "VM ip address"
 }
 
